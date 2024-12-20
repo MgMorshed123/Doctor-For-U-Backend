b92f3099dbe6a669b4be2d7e5c40a5b14a0a1199
mdarf <mdarfinji45@gmail.com>
2024-12-20 19:06:44 +0800

    registerUser contrroller done

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,7 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { userModel } from "../models/userModel";
+import jwt from "jsonwebtoken";
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -7,7 +11,30 @@ export const registerUser = async (req, res) => {
     }
 
     if (!validator.isEmail(email)) {
-      return res.json({ success: false, message: "Missing Details " });
+      return res.json({ success: false, message: "Enter a Valid Email " });
     }
+
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter a Valid Password " });
+    }
+
+    // hash user password
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.jwt_Secret);
+
+    res.json({ success: true, token });
   } catch (error) {}
 };
